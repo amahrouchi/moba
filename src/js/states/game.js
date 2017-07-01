1b7052124cb59b9f3d5b8ff304db757a70874513
Angelo Mahrouchi[user] <angelo.mahrouchi@gmail.com>
2017-07-01 19:07:25 +0200

    Char Selection: Load the map and the player

--- src/js/states/game.js
+++ src/js/states/game.js
@@ -18,14 +18,41 @@ App.States.Game = (function (self) {
      * Preload callback
      */
     self.preload = function () {
-        // console.log('Preload game');
+        game.load.tilemap('tilemap', 'maps/bosquet/ruins.json', null, Phaser.Tilemap.TILED_JSON);
+        game.load.image('mapTiles', 'maps/bosquet/bosquet_tileset.png');
+
+        game.load.spritesheet(self.currentChar.name, self.currentChar.sprite, 32, 48, 16);
     };
 
     /**
      * Create callback
      */
     self.create = function () {
-        // console.log('Create game');
+
+        // Create the map
+        var map = game.add.tilemap('tilemap');
+        map.addTilesetImage('Bosquet', 'mapTiles');
+
+        // Create the layers
+        var groundLayer = map.createLayer('Ground');
+        var aboveLayer = map.createLayer('PlayerAbove');
+        var collisionLayer = map.createLayer('Collision');
+        var behindLayer = map.createLayer('PlayerBehind');
+
+        map.setCollisionBetween(1, 100000, true, 'Collision');
+        groundLayer.resizeWorld();
+
+        //create player
+        var player = game.add.sprite(game.world.centerX, App.HEIGHT - 50, self.currentChar.name, 12);
+        this.game.physics.arcade.enable(this.player);
+
+        //the camera will follow the player in the world
+        // this.game.camera.follow(this.player);
+
+        //move player with cursor keys
+        this.cursors = this.game.input.keyboard.createCursorKeys();
+
+
     };
 
     /**
