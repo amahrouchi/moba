38ab2283b647b18222c08485d85f4eddcd3dc034
Angelo Mahrouchi[user] <angelo.mahrouchi@gmail.com>
2017-07-08 14:08:42 +0200

    Babel build

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,30 @@ var gulp         = require('gulp'),
     sass         = require('gulp-sass'),
     concat       = require('gulp-concat'),
     uglify       = require('gulp-uglify'),
-    minCss       = require('gulp-minify-css')
+    minCss       = require('gulp-minify-css'),
+    sourcemaps   = require('gulp-sourcemaps'),
+    babel        = require('gulp-babel')
 ;
 
 // Build app CSS
 gulp.task('app:sass', function () {
     return gulp.src('./src/scss/**/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass(({outputStyle : 'compressed'})).on('error', sass.logError))
         .pipe(concat('app.css'))
         .pipe(minCss())
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./src/public/css'));
 });
 
 // Build app JS
 gulp.task('app:js', function () {
     return gulp.src('./src/js/**/*.js')
+        .pipe(sourcemaps.init())
         .pipe(concat('app.js'))
+        .pipe(babel())
         .pipe(uglify())
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./src/public/js'));
 });
 
