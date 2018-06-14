d1d6d6295e2f4ea96675590361f489ab9857f5fe
Angelo Mahrouchi <angelo.mahrouchi@omictools.com>
2018-06-14 12:04:42 +0200

    Build JS using babel and sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,42 @@
 'use strict';
 
-var gulp         = require('gulp'),
-    sass         = require('gulp-sass'),
-    concat       = require('gulp-concat'),
-    uglify       = require('gulp-uglify'),
-    minCss       = require('gulp-minify-css')
+var gulp       = require('gulp'),
+    sass       = require('gulp-sass'),
+    concat     = require('gulp-concat'),
+    uglify     = require('gulp-uglify'),
+    minCss     = require('gulp-minify-css'),
+    babel      = require('gulp-babel'),
+    sourcemaps = require('gulp-sourcemaps')
 ;
 
 // Build app CSS
 gulp.task('app:sass', function () {
     return gulp.src('./src/scss/**/*.scss')
-        .pipe(sass(({outputStyle : 'compressed'})).on('error', sass.logError))
-        .pipe(concat('app.css'))
-        .pipe(minCss())
-        .pipe(gulp.dest('./src/public/css'));
+               .pipe(sass(({outputStyle : 'compressed'})).on('error', sass.logError))
+               .pipe(concat('app.css'))
+               .pipe(minCss())
+               .pipe(gulp.dest('./src/public/css'));
 });
 
 // Build app JS
 gulp.task('app:js', function () {
     return gulp.src('./src/js/**/*.js')
-        .pipe(concat('app.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('./src/public/js'));
+               .pipe(sourcemaps.init())
+               .pipe(babel({
+                   presets : ['env']
+               }))
+               .pipe(concat('app.js'))
+               .pipe(uglify())
+               .pipe(sourcemaps.write('.'))
+               .pipe(gulp.dest('./src/public/js'));
 });
 
 // Vendor js
 gulp.task('vendor:js', function () {
     return gulp.src(['./node_modules/phaser-ce/build/phaser.min.js'])
-        .pipe(concat('vendor.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('./src/public/js'));
+               .pipe(concat('vendor.js'))
+               .pipe(uglify())
+               .pipe(gulp.dest('./src/public/js'));
 });
 
 // Default task
